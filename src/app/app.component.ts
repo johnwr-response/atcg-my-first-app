2134c601418d2f19d0478b1c4d39fe5c43fefeb8
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-04-27 16:15:05 +0200

    Assignment 3: Practicing Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,18 @@ import { Component } from '@angular/core';
         h3 {
           color: dodgerblue;
         }
+        .white-text {
+          color: white;
+        }
   `]
 })
 export class AppComponent {
   userName = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
